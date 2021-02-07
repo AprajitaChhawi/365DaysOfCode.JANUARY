c0aa5baf2a1c0f0ef4e0632b81d2a3e0c7a9db31
AprajitaChhawi <achhawip@gmail.com>
2021-02-07 22:33:30 +0530

    Create Day 9 reverse in k group.py

---
+++ Day 9 reverse in k group.py
@@ -0,0 +1,75 @@
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+def reverse(head, k):
+    curr=head
+    next=None
+    prev=None
+    count=0
+    while(curr and count<k):
+        count=count+1
+        next=curr.next
+        curr.next=prev
+        prev=curr
+        curr=next
+    if(next!=None):
+        head.next=reverse(next,k)
+    return prev
+    
+    # Code here
+
+
+
+#{ 
+#  Driver Code Starts
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # self.tail
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = input()
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        k = int(input())
+        new_head = LinkedList()
+        new_head = reverse(llist.head, k)
+        llist.head = new_head
+        llist.printList()
+        t -= 1
+
+
+
+
+# } Driver Code Ends

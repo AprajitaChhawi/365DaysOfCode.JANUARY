c6acab45fffec974677c084ad972fa2e1ac3ea29
AprajitaChhawi <achhawip@gmail.com>
2021-02-07 22:31:51 +0530

    Create Day 1 sum of array elements.py

---
+++ Day 1 sum of array elements.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+def sumElement(arr,n):
+    return sum(arr)
+    #code here
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    testCase=int(input())
+    
+    for _ in range(testCase):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        
+        print(sumElement(arr,n))
+# } Driver Code Ends

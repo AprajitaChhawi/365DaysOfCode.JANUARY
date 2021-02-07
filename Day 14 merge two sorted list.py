9b2e593b17be5b00112d6bf4ee46344a88997440
AprajitaChhawi <achhawip@gmail.com>
2021-02-07 22:34:18 +0530

    Create Day 14 merge two sorted list.py

---
+++ Day 14 merge two sorted list.py
@@ -0,0 +1,88 @@
+#User function Template for python3
+'''
+	Function to merge two sorted lists in one
+	using constant space.
+	
+	Function Arguments: head_a and head_b (head reference of both the sorted lists)
+	Return Type: head of the obtained list after merger.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+'''
+def sortedMerge(head_A, head_B):
+    temp=Node(0)
+    pointer=temp
+    while(head_A and head_B):
+        if head_A.data<=head_B.data:
+            pointer.next=head_A
+            head_A=head_A.next
+        else:
+            pointer.next=head_B
+            head_B=head_B.next
+        pointer=pointer.next
+    if head_A==None:
+        pointer.next=head_B
+    else:
+        pointer.next=head_A
+    return temp.next
+    # code here
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node    
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+        
+# prints the elements of linked list
+def printList(n):
+    while n is not None:
+        print(n.data, end=' ')
+        n = n.next
+    print()
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n,m = map(int, input().strip().split())
+        
+        a = LinkedList() # create a new linked list 'a'.
+        b = LinkedList() # create a new linked list 'b'.
+        
+        nodes_a = list(map(int, input().strip().split()))
+        nodes_b = list(map(int, input().strip().split()))
+        
+        for x in nodes_a:
+            a.append(x)
+        
+        for x in nodes_b:
+            b.append(x)
+        
+        printList(sortedMerge(a.head,b.head))
+
+# } Driver Code Ends
